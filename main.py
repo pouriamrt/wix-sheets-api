63b91915b7b7b5779ffb6866a420b9d8767c16e0
Pouria Mortezaagha <pouriamortezaagha7@gmail.com>
2026-01-23 23:16:26 -0500

    allowed post and option methods as well

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
