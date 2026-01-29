0f40259f90ab9a2a226e71cfe8ee5bb5ce06e9f1
Pouria Mortezaagha <pouriamortezaagha7@gmail.com>
2026-01-28 21:18:49 -0500

    using orjson as default response class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
 
 from api import router
 from config import get_settings
@@ -9,7 +10,7 @@ from config import get_settings
 # Initialize settings to validate environment variables on startup
 get_settings()
 
-app = FastAPI(title="Sheets Live API")
+app = FastAPI(title="Sheets Live API", default_response_class=ORJSONResponse)
 
 # If your Wix site is at a specific domain, replace "*" with that domain for tighter security.
 app.add_middleware(
